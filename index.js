8533e4e29e863390dc02150f288dffd78f9286b7
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-01-11 10:43:14 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
+const { consoleLog } = require("mocha/lib/reporters/base");
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
+for (i=0; i < array.length; i++) {
+    for (j=i+1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }      
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0 (n^2)
 */
 
 /* 
   Add your pseudocode here
+  + Use quadratic programming to iterate element with others (nested for loops)
+    + If statement for comparison to target
+      + Return false outside of all loops to satisfy criteria was not met
 */
 
 /*
   Add written explanation of your solution here
+  + The first for loop is used to hold the index of the first item and slowly move through
+    iteration as we check the trailing items for a match.  If none do we move one index unit
+    and test again.  When conditions are met and the target is matched we return true.  
+    Outside of all the loops we return false as we know no combinations could be reached
+    to match the target.
 */
 
 // You can run `node index.js` to view these console logs
